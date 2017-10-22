5160436cc8ce57bc9c2c1ad561beef7948df39c0
mconceicaonlm <mconceicaonlm@outlook.com>
2017-10-23 00:50:59 +0100

    SiteOneTests_V1

--- AppTestPythoAnyWhere/urls.py
+++ AppTestPythoAnyWhere/urls.py
@@ -1,9 +1,15 @@
-from django.conf.urls import url, include
 from . import views
+from django.conf.urls import url, include
+
 
 #https://docs.djangoproject.com/en/1.11/ref/class-based-views/
 from django.views.generic import ListView, DetailView
 
 urlpatterns = [
     url(r'^$', views.Inicio, name='home'),
-]
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^blog', views.blog, name='blog'),
+    url(r'^photos', views.photos, name='photos'),
+    url(r'^experimentar', views.experimentar, name='experimentar'),
+    url(r'^thanks', views.thanks, name='thanks'),
+    ]
